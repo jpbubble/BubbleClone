50a85832183f7ddd95006bb17ce8153585935d30
Jeroen Broks <facebook@tbbs.nl>
2019-04-24 17:36:25 +0200

    Create CloneBubble.ps1

---
+++ CloneBubble.ps1
@@ -0,0 +1,12 @@
+# Make sure this file is located in the folder where you want all Bubble stuff to live, and run this script there from PowerShell
+# Make sure PowerShell is properly configured to run script
+
+git clone https://github.com/Tricky1975/trickyunits_csharp.git TrickyUnits
+git clone https://github.com/jcr6/JCR6_CSharp.git JCR6
+git clone https://github.com/TrickyGameTools/Kthura.git Kthura
+git clone https://github.com/TrickyGameTools/TQMG.git TQMG
+
+mkdir Bubble
+cd Bubble
+git clone https://github.com/jpbubble/Bubble_Libraries.git
+cd ..
